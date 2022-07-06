f52dae913fbaba26fa5b5c1bcab161c6121ee251
Suravi Sasadara <suravi@pickme.lk>
2022-07-06 12:56:25 +0530

    refactoring

--- src/main/java/com/jps/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/jps/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service")
 @FeignClient(name="netflix-zuul-api-gateway-server")
-@RibbonClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service") //=> no use of this, zuul will do server side load balancing
 public interface CurrencyExchangeServiceProxy {
     //@GetMapping("/currency-exchange/from/{from}/to/{to}")
     @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
